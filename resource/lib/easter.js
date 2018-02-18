43b3566d802f14d4e896019aa1e770cf9dd765fe
Chaspen <chaspen27@gmail.com>
2018-02-18 01:24:57 -0500

    modified:   crp/mus.ogg 	modified:   index.html 	modified:   style.css

--- resource/lib/easter.js
+++ resource/lib/easter.js
@@ -1,4 +1,4 @@
-// a key map of allowed keys
+
 var allowedKeys = {
   37: 'left',
   38: 'up',
@@ -6,30 +6,23 @@ var allowedKeys = {
   40: 'down',
   65: 'a',
   66: 'b'
+    
 };
 
-// the 'official' Konami Code sequence
 var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
 
-// a variable to remember the 'position' the user has reached so far.
 var konamiCodePosition = 0;
 
-// add keydown event listener
 document.addEventListener('keydown', function(e) {
-  // get the value of the key code from the key map
   var key = allowedKeys[e.keyCode];
-  // get the value of the required key from the konami code
   var requiredKey = konamiCode[konamiCodePosition];
 
-  // compare the key with the required key
   if (key == requiredKey) {
 
-    // move to the next key in the konami code sequence
-    konamiCodePosition++;
+    konamiCodePosition++; //get pos
 
-    // if the last key is reached, activate cheats
     if (konamiCodePosition == konamiCode.length) {
-      activateCheats();
+      execEE();
       konamiCodePosition = 0;
     }
   } else {
@@ -37,8 +30,7 @@ document.addEventListener('keydown', function(e) {
   }
 });
 
-function activateCheats() {
+function execEE() {
 
-  window.location = "crp/index.html";
-  alert("FUCK BEES");
+  window.location = "https://www.youtube.com/watch?v=aklgtJDGqvw&feature=youtu.be";
 }
